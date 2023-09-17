205c0dd86e300b587661dd003c205bd847e2e399
Victor Ayala <victor.ayala2a@gmail.com>
2023-09-17 09:55:18 -0400

    Initial Commit

---
+++ n-queens/n_queensApp.swift
@@ -0,0 +1,17 @@
+//
+//  n_queensApp.swift
+//  n-queens
+//
+//  Created by Victor Ayala on 2023-09-17.
+//
+
+import SwiftUI
+
+@main
+struct n_queensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
